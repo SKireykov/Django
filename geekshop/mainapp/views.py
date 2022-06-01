c4a7113136130786ca6e511fff8b35d14751467f
SKireykov <skireykov@mail.ru>
2022-06-01 11:24:27 +0300

    3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,29 +1,25 @@
 from django.shortcuts import render
 
+from mainapp.models import Product, Category
+
 def index(request):
     context = {
-        'current_user': {
-            'first_name': 'Oleg',
-            'last_name' : 'Maslov',
-            'age' : '32'
-        }
+        'products': Product.objects.all()[:4]
     }
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    links_menu = [
-        {'href': 'products_all', 'title': 'все'},
-        {'href': 'products_home', 'title': 'дом'},
-        {'href': 'products_office', 'title': 'офис'},
-        {'href': 'products_modern', 'title': 'модерн'},
-        {'href': 'products_classic', 'title': 'классика'},
-    ]
 
     context = {
-        'links_menu': links_menu,
+        'links_menu': Category.objects.all()
     }
     return render(request, 'mainapp/products.html', context)
 
+def products_list(request, pk):
+    print(pk)
+    context = {
+        'links_menu': Category.objects.all()
+    }
+    return render(request, 'mainapp/products.html', context)
 def contact(request):
-
     return render(request, 'mainapp/contact.html')
