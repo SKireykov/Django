d49a244a8d608b5e80c7162cf52f15b5c5d21959
SKireykov <skireykov@mail.ru>
2022-06-02 18:50:23 +0300

    5

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class ShopUser(AbstractUser):
-    avatar = models.ImageField(upload_to='users', blank=True,verbose_name='Аватарка')
+    avatar = models.ImageField(upload_to='users', blank=True, verbose_name='Аватарка')
     age = models.PositiveSmallIntegerField(verbose_name='Возраст')
